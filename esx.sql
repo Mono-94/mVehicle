27c409562a0ea7269d37c47b9eed05d2b466b7e6
Pegos <stelios@pegos.net>
2024-07-09 12:50:24 +0300

    Update esx.sql

--- esx.sql
+++ esx.sql
@@ -1,6 +1,34 @@
-ALTER TABLE `owned_vehicles`
-ADD COLUMN `metadata` LONGTEXT DEFAULT NULL,
-ADD COLUMN `mileage` INT(11) DEFAULT 0,
-ADD COLUMN `coords` LONGTEXT DEFAULT NULL,
-ADD COLUMN `keys` LONGTEXT DEFAULT '[]',
-ADD COLUMN `lastparking` VARCHAR(100) DEFAULT NULL;
+alter table owned_vehicles
+    add id int first;
+
+alter table owned_vehicles
+    add metadata longtext null;
+
+alter table owned_vehicles
+    add mileage int null;
+
+alter table owned_vehicles
+    add coords longtext null;
+
+alter table owned_vehicles
+    add `keys` longtext null;
+
+alter table owned_vehicles
+    add lastparking varchar(100) null;
+
+create index owned_vehicles_id_index
+    on owned_vehicles (id);
+
+alter table owned_vehicles
+    drop primary key;
+
+alter table owned_vehicles
+    add constraint owned_vehicles_pk
+        primary key (id);
+
+alter table owned_vehicles
+    add constraint owned_vehicles_uk
+        unique (plate);
+
+alter table owned_vehicles
+    modify id int auto_increment;
